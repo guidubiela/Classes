d204fef80f812d50768a81a7c086bc8a2b4a473b
Guilherme <guidubiela@gmail.com>
2023-05-03 16:06:24 -0300

    Do it

---
+++ database/config.inc.php
@@ -0,0 +1,13 @@
+<?php
+    define ('MYSQL_HOST', 'localhost');
+    define ('MYSQL_USUARIO', 'root');
+    define ('MYSQL_SENHA', '');
+    define ('MYSQL_DB', 'curriculo');
+    define ('MYSQL_PORT', '3306');
+
+    define ('MYSQL_DSN', 
+            "mysql:host=".MYSQL_HOST.";
+            port=".MYSQL_PORT.";
+            dbname=".MYSQL_DB.";
+            charset=UTF8");
+?>
